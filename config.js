3903152092a94b12797319dc1af9595e87e296ea
spidies <insperace@gmail.com>
2017-01-22 19:29:02 -0600

    Hello Foo
    Fight the Foo

--- config.js
+++ config.js
@@ -2,17 +2,17 @@ module.exports = {
 
   // Your bot name. Typically, this is your bot's username without the discriminator.
   // i.e: if your bot's username is MemeBot#0420, then this option would be MemeBot.
-  name: "Spidey Bot",
+  name: "SpideyBot",
 
   // The bot's command prefix. The bot will recognize as command any message that begins with it.
   // i.e: "-spidey bot foo" will trigger the command "foo",
   //      whereas "Spidey Bot foo" will do nothing at all.
-  prefix:  "-spidey bot",
+  prefix:  "-spideybot",
 
   // Your bot's user token. If you don't know what that is, go here:
   // https://discordapp.com/developers/applications/me
   // Then create a new application and grab your token.
-  token: "kU1A9EH57FLDLDiWfukn-fkKz0aRLkcoXP75L1yJfxf5xgfSUBycD_ZbLjOLfp5QQlu5",
+  token: "MjcyODc4OTQ4NDYyMTAwNDgx.C2bkXA.S_1wElnmsYcYWNIac6sS-4jt8O4",
 
   // If this option is enabled, the bot will delete the message that triggered it, and its own
   // response, after the specified amount of time has passed.
